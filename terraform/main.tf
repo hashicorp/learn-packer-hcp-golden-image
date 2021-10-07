a29fc6382b6bd34127936df7b75033455b30aec1
Anthony <russo555@gmail.com>
2021-10-07 16:02:35 -0400

    Removing commented code.

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,27 +12,6 @@ data "hcp_packer_image" "loki" {
   region         = "us-east-2"
 }
 
-/*
-data "hcp_packer_iteration" "hashicups" {
-  bucket_name = var.hcp_bucket_hashicups
-  channel     = var.hcp_channel
-}
-
-data "hcp_packer_image" "hashicups_west" {
-  bucket_name    = var.hcp_bucket_hashicups
-  cloud_provider = "aws"
-  iteration_id   = data.hcp_packer_iteration.hashicups.ulid
-  region         = "us-west-2"
-}
-
-data "hcp_packer_image" "hashicups_east" {
-  bucket_name    = var.hcp_bucket_hashicups
-  cloud_provider = "aws"
-  iteration_id   = data.hcp_packer_iteration.hashicups.ulid
-  region         = "us-east-2"
-}
-*/
-
 provider "aws" {
   region = var.region_east
 }
@@ -59,48 +38,3 @@ resource "aws_instance" "loki" {
     Name = "Learn-Packer-LokiGrafana"
   }
 }
-
-/*
-resource "aws_instance" "hashicups_east" {
-  ami           = data.hcp_packer_image.hashicups_east.cloud_image_id
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.subnet_public_east.id
-  vpc_security_group_ids = [
-    aws_security_group.ssh_east.id,
-    aws_security_group.allow_egress_east.id,
-    aws_security_group.promtail_east.id,
-    aws_security_group.hashicups_east.id,
-  ]
-  associate_public_ip_address = true
-
-  tags = {
-    Name = "Learn-Packer-HashiCups"
-  }
-
-  depends_on = [
-    aws_instance.loki
-  ]
-}
-
-resource "aws_instance" "hashicups_west" {
-  provider      = aws.west
-  ami           = data.hcp_packer_image.hashicups_west.cloud_image_id
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.subnet_public_west.id
-  vpc_security_group_ids = [
-    aws_security_group.ssh_west.id,
-    aws_security_group.allow_egress_west.id,
-    aws_security_group.promtail_west.id,
-    aws_security_group.hashicups_west.id,
-  ]
-  associate_public_ip_address = true
-
-  tags = {
-    Name = "Learn-Packer-HashiCups"
-  }
-
-  depends_on = [
-    aws_instance.loki
-  ]
-}
-*/
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,15 +2,3 @@ output "loki_ip" {
   value = aws_instance.loki.public_ip
   description = "Public IP address for the Loki and Grafana instance."
 }
-
-/*
-output "hashicups_east_ip" {
-  value = aws_instance.hashicups_east.public_ip
-  description = "Public IP address for the HashiCups instance in us-east-2."
-}
-
-output "hashicups_west_ip" {
-  value = aws_instance.hashicups_west.public_ip
-  description = "Public IP address for the HashiCups instance in us-west-2."
-}
-*/

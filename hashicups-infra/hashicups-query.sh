ca0f0ff0cb842bf1a7bcaa84604385ac48a4a23a
Robin Norwood <rnorwood@hashicorp.com>
2024-05-21 10:43:34 -0500

    Update directory name, include hashicups infra commented for ease of use in lab

---
+++ hashicups-infra/hashicups-query.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Get IPs from terraform output
+HASHICUPS_IP_EAST=$(terraform output -raw hashicups_east_ip)
+HASHICUPS_IP_WEST=$(terraform output -raw hashicups_west_ip)
+
+# Interval in seconds
+CURL_INTERVAL=5
+
+echo "HashiCups address (EAST): $HASHICUPS_IP_EAST"
+echo "HashiCups address (WEST): $HASHICUPS_IP_WEST"
+echo -e "Making requests to hashicups services every $CURL_INTERVAL seconds.\nPress ctrl+c to quit."
+
+while true; do 
+    echo -e "\n\nHashiCups (EAST) response:"
+    curl $HASHICUPS_IP_EAST:19090/coffees; sleep $CURL_INTERVAL; 
+    echo -e "\n\nHashiCups (WEST) response:"
+    curl $HASHICUPS_IP_WEST:19090/coffees; sleep $CURL_INTERVAL; 
+done
---
+++ hashicups-infra/variables.tf
@@ -0,0 +1,49 @@
+variable "cidr_vpc_east" {
+  description = "CIDR block for the VPC"
+  default     = "10.1.0.0/16"
+}
+
+variable "cidr_subnet_east" {
+  description = "CIDR block for the subnet"
+  default     = "10.1.0.0/24"
+}
+
+variable "cidr_vpc_west" {
+  description = "CIDR block for the VPC"
+  default     = "10.2.0.0/16"
+}
+
+variable "cidr_subnet_west" {
+  description = "CIDR block for the subnet"
+  default     = "10.2.0.0/24"
+}
+
+variable "environment_tag" {
+  description = "Environment tag"
+  default     = "Learn"
+}
+
+variable "region_east" {
+  description = "The default region where Terraform deploys your resources"
+  default     = "us-east-2"
+}
+
+variable "region_west" {
+  description = "The second region where Terraform deploys your resources"
+  default     = "us-west-2"
+}
+
+variable "hcp_bucket_loki" {
+  description = "HCP Packer bucket name for loki image"
+  default     = "learn-packer-hcp-loki-image"
+}
+
+variable "hcp_bucket_hashicups" {
+  description = "HCP Packer bucket name for base golden image"
+  default     = "learn-packer-hcp-hashicups-image"
+}
+
+variable "hcp_channel" {
+  description = "HCP Packer channel name"
+  default     = "Production"
+}

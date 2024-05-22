4517d9e2f5a28ddae4271f9fb715385f4c67e16a
Brian McClain <brianmmcclain@gmail.com>
2024-05-22 01:01:36 -0400

    Bump Promtail version, match Loki docker plugin version

--- golden/setup-promtail.sh
+++ golden/setup-promtail.sh
@@ -1,14 +1,11 @@
 # Set up promtail
 cd /opt/promtail
-sudo curl -O -L "https://github.com/grafana/loki/releases/download/v1.5.0/promtail-linux-amd64.zip"
+sudo curl -O -L "https://github.com/grafana/loki/releases/download/v2.9.8/promtail-linux-amd64.zip"
 sudo unzip "promtail-linux-amd64.zip"
 sudo chmod a+x "promtail-linux-amd64"
 
 ## Configure Docker with promtail
-sudo docker plugin install grafana/loki-docker-driver:latest --alias loki --grant-all-permissions
-sudo docker plugin disable loki --force
-sudo docker plugin upgrade loki grafana/loki-docker-driver:latest --grant-all-permissions
-sudo docker plugin enable loki
+sudo docker plugin install grafana/loki-docker-driver:2.9.8 --alias loki --grant-all-permissions
 
 # Restart auditd
 sudo systemctl restart auditd
